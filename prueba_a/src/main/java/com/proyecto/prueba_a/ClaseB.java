297cafa65d198cd10d9498b5aeec5b68fd49dca6
juanma <juan-ma@telefonica.net>
2012-11-01 12:36:37 +0100

    claseB

---
+++ prueba_a/src/main/java/com/proyecto/prueba_a/ClaseB.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.proyecto.prueba_a;
+
+/**
+ *
+ * @author juanma
+ */
+public class ClaseB {
+    
+}
